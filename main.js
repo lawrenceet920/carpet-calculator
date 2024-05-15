26c63b2fda0a0d2762098948761f572fa1375864
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-05-15 12:59:45 -0400

    updated and improved JS

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ document.getElementById("calculate").addEventListener("click", calc)
 
 function calc() {
     // Imput
-    const width = document.getElementById('width').value;
-    const length = document.getElementById('length').value;
-    const price = document.getElementById('price').value;
+    const width = +document.getElementById('width').value;
+    const length = +document.getElementById('length').value;
+    const price = +document.getElementById('price').value;
 
     // Process
-    const totalPrice = width * length * price;
+    const totalPrice = '$' + (width * length * price).toFixed(2);
 
     // Output
     console.log (`The total of width, height and, price multiplied is ${totalPrice}`)
-    document.getElementById('output').innerHTML = `A carpet with an area of <em>${width * length}</em> and a price of <em>$${price}</em>ft<sup>2</sup> will have a value of <strong>$${totalPrice}</strong>`;
+    document.getElementById('output').innerHTML = `A carpet with an area of <em>${width * length}</em>ft<sup>2</sup> and a price of $<em>${price}</em>ft<sup>2</sup> will have a value of: <h3>${totalPrice}</h3>`;
 }
